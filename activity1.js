68d236ff9b5a4abc0699fb5581a424aba8442093
SAGARA,Lee Alfie A. <leealfiesagara03@gmail.com>
2025-02-24 13:58:07 +0800

    Create activity1.js

---
+++ activity1.js
@@ -0,0 +1,9 @@
+let name = "YourName";
+let whatDoYouWannaBecomeInYourLife = "programmer";
+let gender = "YourGender";
+let twitterHandle = "@YourTwitterHandle";
+
+console.log("Name:", name);
+console.log("What do you wanna become in your life:", whatDoYouWannaBecomeInYourLife);
+console.log("Gender:", gender);
+console.log("Twitter Handle:", twitterHandle);
