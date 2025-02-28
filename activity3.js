cee894c031f2ced96fdc2273521767c9f3aec69f
SAGARA,Lee Alfie A. <leealfiesagara03@gmail.com>
2025-03-01 00:14:14 +0800

    activity3.js

---
+++ activity3.js
@@ -0,0 +1,17 @@
+// 1. Create a variable named isJsProgrammingLanguage and store true.
+let isJsProgrammingLanguage = true;
+
+// 2. Create a variable named isJsHard and store false.
+let isJsHard = false;
+
+// 3. Create a variable named favNumb and store your favorite number.
+let favNumb = 15; // Change this to your favorite number
+
+// 4. Add favNumb with the value of undefined.
+let result = favNumb + undefined;
+
+// 5. Log all variables to the console.
+console.log("Is JavaScript a programming language? ", isJsProgrammingLanguage);
+console.log("Is JavaScript hard? ", isJsHard);
+console.log("Favorite Number: ", favNumb);
+console.log("Result of adding favNumb with undefined: ", result);
